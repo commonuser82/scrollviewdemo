1dd60d4f29804236897f95a75358a477a5b13138
“shreenashah” <shreenashah92@gmail.com>
2017-01-06 16:10:15 +0530

    Initial Commit

---
+++ ScrollviewAutolayoutdemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollviewAutolayoutdemo
+//
+//  Created by shreena shah on 06/01/17.
+//  Copyright © 2017 shreena shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
